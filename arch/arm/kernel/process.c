3d091ffd42e7b7dfc0c5b68e277c1911e60d4754
Yauhen Kharuzhy <jekhor@gmail.com>
2008-12-03 16:12:48 +0200

    Remove including of regs-gpio.h
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -40,7 +40,6 @@
 #include <asm/stacktrace.h>
 #include <asm/mach/time.h>
 #include <mach/hardware.h>
-#include <mach/regs-gpio.h>
 
 static const char *processor_modes[] = {
   "USER_26", "FIQ_26" , "IRQ_26" , "SVC_26" , "UK4_26" , "UK5_26" , "UK6_26" , "UK7_26" ,
