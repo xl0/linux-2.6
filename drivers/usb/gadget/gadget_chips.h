6e7aa75743a34554e9f5d5501e18bd78a8e0c906
Yauhen Kharuzhy <jekhor@gmail.com>
2009-01-22 19:01:32 +0200

    JZ: add USB gadget controller driver
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -15,6 +15,18 @@
 #ifndef __GADGET_CHIPS_H
 #define __GADGET_CHIPS_H
 
+#ifdef CONFIG_USB_GADGET_JZ4740
+#define	gadget_is_jz4740(g)	!strcmp("jz4740_udc", (g)->name)
+#else
+#define	gadget_is_jz4740(g)	0
+#endif
+
+#ifdef CONFIG_USB_GADGET_JZ4730
+#define	gadget_is_jz4730(g)	!strcmp("jz4730_udc", (g)->name)
+#else
+#define	gadget_is_jz4730(g)	0
+#endif
+
 #ifdef CONFIG_USB_GADGET_NET2280
 #define	gadget_is_net2280(g)	!strcmp("net2280", (g)->name)
 #else
@@ -254,7 +266,10 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 	else if (gadget_is_r8a66597(gadget))
 		return 0x25;
 	else if (gadget_is_s1r72v17(gadget))
+	else if (gadget_is_jz4730(gadget))
 		return 0x26;
+	else if (gadget_is_jz4740(gadget))
+		return 0x27;
 	return -ENOENT;
 }
 
