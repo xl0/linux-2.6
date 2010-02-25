ed77ad987ba40a4a11a50eb704b1784db545b3fc
Yauhen Kharuzhy <jekhor@gmail.com>
2010-02-25 22:48:58 +0200

    n516-lpc: Report 7% instead 0% on start without LPC messages
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- drivers/i2c/chips/n516-lpc.c
+++ drivers/i2c/chips/n516-lpc.c
@@ -367,6 +367,9 @@ static int __devinit n516_lpc_probe(struct i2c_client *client, const struct i2c_
 	chip->i2c_client = client;
 	if ((batt_level > 0) && (batt_level < ARRAY_SIZE(batt_charge)))
 		chip->battery_level = batt_level;
+	else
+		chip->battery_level = 1;
+
 	INIT_WORK(&chip->work, n516_lpc_read_keys);
 	i2c_set_clientdata(client, chip);
 
