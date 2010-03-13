31804bdc2c816626ba90ac7dc8ea71e9cd04e143
Yauhen Kharuzhy <jekhor@gmail.com>
2010-03-13 23:39:23 +0200

    N516: Fix battery power supply name in GPIO charger platform data
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- arch/mips/jz4740/board-n516.c
+++ arch/mips/jz4740/board-n516.c
@@ -111,7 +111,7 @@ static struct jz_nand_platform_data n516_nand_pdata = {
 };
 
 static char *n516_batteries[] = {
-	"n516_battery",
+	"n516-battery",
 };
 
 static struct gpio_charger_platform_data n516_charger_pdata = {
